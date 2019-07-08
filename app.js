557a2db4c39d63020f71cd89de50f2027e2bf9c8
Eric Njogu <kunadawa@gmail.com>
2019-07-08 16:33:41 +0300

    feat: handling HTTP GET for the /books route

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
-
 const app = express();
-
+const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+const booksPath = '/books';
+
+bookRouter.route(booksPath).get((req, resp) => {
+  const obj = {msg:'from the /books route'};
+  resp.json(obj);
+})
+
+app.use(bookRouter);
 
 app.get('/', (request, response) => {
   response.send('Welcome to Embakasi, Sir!');
