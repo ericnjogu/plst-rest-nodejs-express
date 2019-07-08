230395c65b9f042238190a0bdaec582f8d0b7e77
Eric Njogu <kunadawa@gmail.com>
2019-07-08 14:19:36 +0300

    fix: using ES5 require
    adding the --experimental-modules option would not work becaue the file 
    would additionaly need to have a .mjs extension, which would break 
    linting :(

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import express from 'express';
+const express = require('express');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.get('/', (request, response) => {
-  response.send('Welcome to Embakasi!');
+  response.send('Welcome to Embakasi, Sir!');
 });
 
 app.listen(port, () => {
