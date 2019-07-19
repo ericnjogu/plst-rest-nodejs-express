3c71b29c992c7ea3efe4846e5a2c3ff8e98a7fc4
Eric Njogu <kunadawa@gmail.com>
2019-07-19 14:25:01 +0300

    feat: using body parser to retrieve post JSON and create model
    - echoing the sent JSON for now
    - if the request MIME type is not application/json, some properties are 
    left out

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const body_parser = require('body-parser')
 
 const app = express();
 const bookRouter = express.Router();
@@ -9,20 +10,29 @@ const booksPath = '/books';
 const db = mongoose.connect('mongodb://localhost:27017/book_api', {useNewUrlParser:true});
 const Book = require('./models/Book');
 
-bookRouter.route(booksPath).get((req, resp) => {
-  let valid_query = {}
-  Object.keys(Book.schema.obj)
-    .filter(
-      (prop) => (req.query[prop]))
-    .map(
-    (prop) => {
-        valid_query[prop] = {$regex:req.query[prop], $options:'i'}
-    }
-  );
-  //console.log(valid_query);
-  Book.find(valid_query, (err, books) => {
-    return err ? resp.send(err) : resp.json(books);
+app.use(body_parser.urlencoded({extended:true}));
+app.use(body_parser.json());
+
+bookRouter.route(booksPath)
+  .post((req, resp) => {
+    const new_book = new Book(req.body);
+    console.log(`posted book ${new_book}`);
+    return resp.send(new_book);
   })
+  .get((req, resp) => {
+    let valid_query = {}
+    Object.keys(Book.schema.obj)
+      .filter(
+        (prop) => (req.query[prop]))
+      .map(
+      (prop) => {
+          valid_query[prop] = {$regex:req.query[prop], $options:'i'}
+      }
+    );
+    //console.log(valid_query);
+    Book.find(valid_query, (err, books) => {
+      return err ? resp.send(err) : resp.json(books);
+    })
 });
 
 const path_book_id = '/books/:id';
