e4e369400bf6423b93568d5c3e9669620ff3d449
Eric Njogu <kunadawa@gmail.com>
2019-07-19 14:52:36 +0300

    chore: moving router code into separate module

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose');
 const body_parser = require('body-parser')
 
 const app = express();
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
-const booksPath = '/books';
+
 // resolved url parsing error messages by following https://stackoverflow.com/a/51935257/315385
 const db = mongoose.connect('mongodb://localhost:27017/book_api', {useNewUrlParser:true});
 const Book = require('./models/Book');
@@ -13,41 +12,8 @@ const Book = require('./models/Book');
 app.use(body_parser.urlencoded({extended:true}));
 app.use(body_parser.json());
 
-bookRouter.route(booksPath)
-  .post((req, resp) => {
-    const new_book = new Book(req.body);
-    console.log(`posted book ${new_book}`);
-    new_book.save();
-    return resp.status(201).send(new_book);
-  })
-  .get((req, resp) => {
-    let valid_query = {}
-    Object.keys(Book.schema.obj)
-      .filter(
-        (prop) => (req.query[prop]))
-      .map(
-      (prop) => {
-          valid_query[prop] = {$regex:req.query[prop], $options:'i'}
-      }
-    );
-    //console.log(valid_query);
-    Book.find(valid_query, (err, books) => {
-      return err ? resp.send(err) : resp.json(books);
-    })
-});
-
-const path_book_id = '/books/:id';
-const bookIdRouter = express.Router();
-bookIdRouter.route(path_book_id).get((req, resp) => {
-  //console.log(`finding by id ${req.params.id}`);
-  Book.findById(req.params.id, (err, book) => {
-      return err ? resp.send(err) : resp.json(book);
-    }
-  )
-})
-
+const bookRouter = require('./routes/books-router')();
 app.use(bookRouter);
-app.use(bookIdRouter);
 
 app.get('/', (request, response) => {
   response.send('Welcome to Embakasi, Sir!');
---
+++ routes/books-router.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const Book = require('../models/Book');
+
+function router() {
+  const booksPath = '/books';
+  const bookRouter = express.Router();
+  bookRouter.route(booksPath)
+    .post((req, resp) => {
+      const new_book = new Book(req.body);
+      console.log(`posted book ${new_book}`);
+      new_book.save();
+      return resp.status(201).send(new_book);
+    })
+    .get((req, resp) => {
+      let valid_query = {}
+      Object.keys(Book.schema.obj)
+        .filter(
+          (prop) => (req.query[prop]))
+        .map(
+        (prop) => {
+            valid_query[prop] = {$regex:req.query[prop], $options:'i'}
+        }
+      );
+      //console.log(valid_query);
+      Book.find(valid_query, (err, books) => {
+        return err ? resp.send(err) : resp.json(books);
+      })
+  });
+
+  const path_book_id = '/books/:id';
+  bookRouter.route(path_book_id).get((req, resp) => {
+    //console.log(`finding by id ${req.params.id}`);
+    Book.findById(req.params.id, (err, book) => {
+        return err ? resp.send(err) : resp.json(book);
+      }
+    )
+  })
+
+  return bookRouter;
+}
+
+module.exports = router;
