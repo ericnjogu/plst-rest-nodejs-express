5d9d13b60018ee51db923f6f5a978dde6fb72a6b
Eric Njogu <kunadawa@gmail.com>
2019-07-19 10:05:24 +0300

    chore: initial book list
    inserted into mongodb using `mongo book_api < initial_books.js`

---
+++ initial_books.js
@@ -0,0 +1,28 @@
+db.books.insert (
+  [
+    {
+      title:'Dangerous personalities',
+      author:'Joe Navarro',
+      genre:'psychology',
+      read:false
+    },
+    {
+      title:'The wisdom of psychopaths',
+      author:'Dr Kevin Dutton',
+      genre:'psychology',
+      read:false
+    },
+    {
+      title:'Cherry',
+      author:'Nico Walker',
+      genre:'autobiography',
+      read:false
+    },
+    {
+      title:'Shadow divers',
+      author:'Robert Kurson',
+      genre:'non-fiction',
+      read:false
+    },
+  ]
+)
