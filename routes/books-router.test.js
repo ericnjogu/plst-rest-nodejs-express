3890126ea3e083c8290c3f7e2eb48d6bd944db2a
Eric Njogu <kunadawa@gmail.com>
2019-07-24 11:13:24 +0300

    refactor: using promise based generator to return discoverd schema props
    Tried this out to see if it would help in testing a scenario where a 
    strange prop should not be discovered but the test did not pass

--- routes/books-router.test.js
+++ routes/books-router.test.js
@@ -1,12 +1,32 @@
 const bookRouter = require('./books-router');
 const book_schema = require('../models/Book').schema;
 
-test('update func is called with correct source value', done => {
-  function callback(key, value) {
-    expect(key).toBe('author');
-    expect(value).toBe('Mimi');
-    done();
+test('update func is called with correct source value', () => {
+  const generator =  bookRouter.discoverSchemaProps(book_schema, {author: 'Mimi'});
+  let looping = true;
+  while (looping) {
+    const {value_promise, done} = generator.next();
+    looping = !done;
+    value_promise ? value_promise.then((key, value) => {
+      expect(key).toBe('author');
+      expect(value).toBe('Mimi');
+    }) : () => {};
   }
+});
 
-  bookRouter.discoverSchemaProps(book_schema, {author: 'Mimi'}, callback);
+test('update func should not be called for non existent props', done => {
+  const generator =  bookRouter.discoverSchemaProps(book_schema, {foofoo: 'Bibi'});
+  let looping = true;
+  while (looping) {
+    const {value_promise, done} = generator.next();
+    looping = !done;
+    if (value_promise) {
+      value_promise.then(
+        (key, value) => {
+          fail(`Should not be discovered: ${key}:${value}`);
+        },
+        (key) => {expect(key).toBe('foofoo');}
+      );
+    }
+  }
 });
