8675dbf79eaf44f0bbbe2509eaa3c09be346fb10
Eric Njogu <kunadawa@gmail.com>
2019-07-24 16:10:40 +0300

    feat: attempting to return a promise in a callback function
    The tests passed but the router code failed since the callback receiving 
    a promise returns immediately and an empty query is passed to mongodb

--- routes/books-router.js
+++ routes/books-router.js
@@ -1,20 +1,20 @@
 const express = require('express');
-
+const book_schema = require('../models/Book').schema;
 /**
-* function that creates a  generator that iterates through an object that contains the specified properties
-* inspired by https://www.codementor.io/tiagolopesferreira/asynchronous-iterators-in-javascript-jl1yg8la1
+* function that iterates through an object that contains the specified schema properties
+* inspired by https://www.freecodecamp.org/news/how-to-make-a-promise-out-of-a-callback-function-in-javascript-d8ec35d1f981/
 * @param {Object} schema - object schema
 * @param {Object} source - source object to iterate through
 * @callback {callback} - function called with discovered property name and value
+@ @return {Object} -  a promise
 */
-function* discoverSchemaProps(schema, source) {
-  const keys = Object.keys(source);
-  while (keys.length) {
-    key = keys.shift();
-    yield new Promise((resolve, reject) => {
-      schema[key] ? resolve(key, source[key]) : reject(key);
-    });
-  }
+function discoverSchemaProps(schema, source, callback) {
+  Object.keys(source).map(key => (
+      callback(new Promise((resolve, reject) => {
+        schema[key] ? resolve(key, source[key]) : reject(key);
+      }))
+    )
+  );
 }
 
 function router(Book) {
@@ -29,17 +29,17 @@ function router(Book) {
     })
     .get((req, resp) => {
       let valid_query = {};
-      const generator =  discoverSchemaProps(Book.schema.obj, {author: 'Mimi'});
-      let looping = true;
-      while (looping) {
-        const {value_promise, done} = generator.next();
-        looping = !done;
-        value_promise ? value_promise.then((key, value) => {
+      const {query} = req;
+      discoverSchemaProps(book_schema, query, (promise) => {
+        promise.then((key, value) => {
+          console.log(`updating valid_query with ${key}:${value}`);
           valid_query[key] = {$regex:value, $options:'i'}
-        }) : () => {};
-      }
+        }).catch((key) => {
+          console.error (`property '${key}' is not present in the book schema`);
+        });
+      });
 
-      //console.log(valid_query);
+      console.log(valid_query);
       Book.find(valid_query, (err, books) => {
         return err ? resp.send(err) : resp.json(books);
       });
